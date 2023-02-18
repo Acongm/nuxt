08a3a10076cc6c3a690cc902c4c67a83ea47fb3a
acongm <acongm@126.com>
2023-02-18 22:00:41 +0800

    feature: 项目文件demo

---
+++ src/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- <h1>这里是 nuxt 的默认模板，可以添加在所有页面，也可单独添加</h1> -->
+    <!-- <h1>欢迎来到 Nuxt 的世界</h1> -->
+
+    <!-- <NuxtLink to="/list">进入列表页</NuxtLink> -->
+    <!-- <NuxtLink to="/[id]">进入详情页</NuxtLink> -->
+    <slot />
+  </div>
+</template>
---
+++ src/pages/list/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>欢迎来到acongm的列表页面</h1>
+    <ListItem></ListItem>
+  </div>
+</template>
